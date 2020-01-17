b2b5a4154525ca28241428bad366e56504f073e9
Nicola Altini <nicola.altini@poliba.it>
2020-01-17 17:56:11 +0100

    + inference for hv/pv

--- projects/liver/inference/inference_vessels.py
+++ projects/liver/inference/inference_vessels.py
@@ -16,15 +16,21 @@ folders_patients = os.listdir(folder_dataset)
 folders_patients_train = [folder for folder in folders_patients if not any(val_el in folder for val_el in val_list)]
 folders_patients_valid = [folder for folder in folders_patients if any(val_el in folder for val_el in val_list)]
 
+path_net_hv = 'logs/hv/model_25D__2020-01-16__16_42_37.pht'
+path_net_pv = 'logs/pv/model_25D__2020-01-17__16_42_59.pht'
 path_net = 'logs/vessels/model_25D__2020-01-15__08_28_39.pht'
 # Load net
 net = torch.load(path_net)
+net_hv = torch.load(path_net_hv)
+net_pv = torch.load(path_net_pv)
 
 # Start iteration over val set
 for folder_patient_valid in folders_patients_valid:
 
     path_test_image = os.path.join(folder_dataset, folder_patient_valid, 'image', 'image.nii')
     path_test_pred  = os.path.join(folder_dataset, folder_patient_valid, 'image', 'pred.nii')
+    path_test_pred_hv = os.path.join(folder_dataset, folder_patient_valid, 'image', 'pred_hv.nii')
+    path_test_pred_pv = os.path.join(folder_dataset, folder_patient_valid, 'image', 'pred_pv.nii')
 
     # load file
     data = nib.load(path_test_image)
@@ -34,7 +40,7 @@ for folder_patient_valid in folders_patients_valid:
     data = data.get_data()
 
     # normalize data
-    # data = normalize_data(data, dmin=window_hu[0], dmax=window_hu[1])
+    data = normalize_data(data, dmin=window_hu[0], dmax=window_hu[1])
 
     # transpose so the z-axis (slices) are the first dimension
     data = np.transpose(data, (2, 0, 1))
@@ -42,8 +48,19 @@ for folder_patient_valid in folders_patients_valid:
     output = perform_inference_volumetric_image(net, data, context=2, do_round=True)
     output = np.transpose(output, (1, 2, 0)).astype(np.uint8)
 
+    # HV / PV
+    output_hv = perform_inference_volumetric_image(net_hv, data, context=2, do_round=True)
+    output_pv = perform_inference_volumetric_image(net_pv, data, context=2, do_round=True)
+    output_hv = np.transpose(output_hv, (1, 2, 0)).astype(np.uint8)
+    output_pv = np.transpose(output_pv, (1, 2, 0)).astype(np.uint8)
+
     n_nonzero = np.count_nonzero(output)
     print("Non-zero elements = ", n_nonzero)
+
+    output_nib_hv = nib.Nifti1Image(output_hv, affine=input_aff)
+    output_nib_pv = nib.Nifti1Image(output_pv, affine=input_aff)
+    nib.save(output_nib_hv, path_test_pred_hv)
+    nib.save(output_nib_pv, path_test_pred_pv)
 
     output_nib_pre = nib.Nifti1Image(output, affine=input_aff)
     nib.save(output_nib_pre, path_test_pred)
@@ -70,6 +87,32 @@ for folder_patient_valid in folders_patients_valid:
     print('Precision = ', prec)
     print('Recall    = ', recall)
     print('RVD       = ', rvd)
+
+    iou_hv = mmb.jc(output_hv, gt_hv_mask)
+    dice_hv = mmb.dc(output_hv, gt_hv_mask)
+    prec_hv = mmb.precision(output_hv, gt_hv_mask)
+    recall_hv = mmb.recall(output_hv, gt_hv_mask)
+    rvd_hv = mmb.ravd(output_hv, gt_hv_mask)
+
+    print('Hepatic Vein')
+    print('IoU       = ', iou_hv)
+    print('Dice      = ', dice_hv)
+    print('Precision = ', prec_hv)
+    print('Recall    = ', recall_hv)
+    print('RVD       = ', rvd_hv)
+
+    iou_pv = mmb.jc(output_hv, gt_hv_mask)
+    dice_pv = mmb.dc(output_hv, gt_hv_mask)
+    prec_pv = mmb.precision(output_hv, gt_hv_mask)
+    recall_pv = mmb.recall(output_hv, gt_hv_mask)
+    rvd_pv = mmb.ravd(output_hv, gt_hv_mask)
+
+    print('Portal Vein')
+    print('IoU       = ', iou_pv)
+    print('Dice      = ', dice_pv)
+    print('Precision = ', prec_pv)
+    print('Recall    = ', recall_pv)
+    print('RVD       = ', rvd_pv)
 
 # TODO: post processing
 # Connected components labeling
