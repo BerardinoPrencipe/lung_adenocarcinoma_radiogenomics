58c33dc73424271ad47b8ee80b0eb23a701ecae9
Nicola Altini <nicola.altini@poliba.it>
2020-01-22 15:06:16 +0100

    + Tversky loss

--- projects/liver/inference/inference.py
+++ projects/liver/inference/inference.py
@@ -73,9 +73,11 @@ if use_traced_model:
 else:
     net = torch.load(os.path.join(logs_dir, "model_" + model_name + ".pht"))
     if cuda and use_multi_gpu: net = use_multi_gpu_model(net)
+    elif cuda: net = net.cuda()
     net.eval()  # inference mode
 
-for file_name_prediction in files_test_volumes[:1]:
+
+for file_name_prediction in files_test_volumes:
     # load file
     data = nib.load(os.path.join(test_folder, file_name_prediction))
 
@@ -118,17 +120,29 @@ if inference_on_test_set:
     val_list = [idx for idx in range(len(files_test_volumes))]
 else:
     val_list = [idx for idx in range(20)]
+if use_sliver_as_valid: val_list = [idx for idx in range(1,21)]
 
 ious_pre   = np.zeros(len(val_list))
 ious_post  = np.zeros(len(val_list))
+
 dices_pre  = np.zeros(len(val_list))
 dices_post = np.zeros(len(val_list))
+
 rvds_pre   = np.zeros(len(val_list))
 rvds_post  = np.zeros(len(val_list))
+
+
 assds_pre  = np.zeros(len(val_list))
 assds_post = np.zeros(len(val_list))
+
 hds_pre    = np.zeros(len(val_list))
 hds_post   = np.zeros(len(val_list))
+
+ious_rg    = np.zeros(len(val_list))
+dices_rg   = np.zeros(len(val_list))
+rvds_rg    = np.zeros(len(val_list))
+assds_rg   = np.zeros(len(val_list))
+hds_rg     = np.zeros(len(val_list))
 
 paths_predictions_pre  = [filename for filename in os.listdir(results_folder_pre) if filename.endswith(".nii")]
 paths_predictions_post = [filename for filename in os.listdir(results_folder_post) if filename.endswith(".nii")]
@@ -138,65 +152,97 @@ if use_sliver_as_valid:
     sliver_masks_folder = 'E:/Datasets/Sliver_Nifti/GroundTruth'
     paths_ground_truth = [file for file in os.listdir(sliver_masks_folder)]
 
+region_growing_pred_folder = 'E:/Datasets/Sliver_Nifti/Results/RegionGrowing/D25'
+paths_predictions_rg = [filename for filename in os.listdir(region_growing_pred_folder)]
+
 paths_predictions_pre.sort()
 paths_predictions_post.sort()
 paths_ground_truth.sort()
+paths_predictions_rg.sort()
 
-for idx, (path_prediction_pre, path_prediction_post, path_ground_truth) in \
-        enumerate(zip(paths_predictions_pre, paths_predictions_post, paths_ground_truth)):
+eval_cnn = False
+eval_rg  = True
+
+for idx, (path_prediction_pre, path_prediction_post, path_prediction_rg, path_ground_truth) in \
+        enumerate(zip(paths_predictions_pre, paths_predictions_post, paths_predictions_rg, paths_ground_truth)):
 
     print("Index: ", idx)
-    p_id = get_patient_id(path_ground_truth)
-    if p_id not in val_list:
-        continue
+    if not use_sliver_as_valid:
+        p_id = get_patient_id(path_ground_truth)
+        if p_id not in val_list:
+            continue
+        else:
+            print("Patient ID = ", p_id)
     else:
-        print("Patient ID = ", p_id)
+        p_id = idx
 
     prediction_mask_pre  = nib.load(os.path.join(results_folder_pre, path_prediction_pre))
     prediction_mask_pre  = prediction_mask_pre.get_data()
     prediction_mask_post = nib.load(os.path.join(results_folder_post, path_prediction_post))
     prediction_mask_post = prediction_mask_post.get_data()
-    ground_truth_mask    = nib.load(os.path.join(test_folder, path_ground_truth))
+    path_gt_mask = os.path.join(test_folder, path_ground_truth)
+    if use_sliver_as_valid: path_gt_mask = os.path.join(sliver_masks_folder, path_ground_truth)
+
+    prediction_mask_rg = nib.load(os.path.join(region_growing_pred_folder, path_prediction_rg))
+    prediction_mask_rg =  prediction_mask_rg.get_data()
+    prediction_mask_rg[prediction_mask_rg == prediction_mask_rg.min()] = 0
+    prediction_mask_rg[prediction_mask_rg == prediction_mask_rg.max()] = 1
+
+    ground_truth_mask = nib.load(path_gt_mask)
     voxel_spacing        = ground_truth_mask.header.get_zooms()
     ground_truth_mask    = ground_truth_mask.get_data()
 
-    ious_pre[p_id]   = mmb.jc(prediction_mask_pre, ground_truth_mask)
-    ious_post[p_id]  = mmb.jc(prediction_mask_post, ground_truth_mask)
 
-    dices_pre[p_id]  = mmb.dc(prediction_mask_pre, ground_truth_mask)
-    dices_post[p_id] = mmb.dc(prediction_mask_post, ground_truth_mask)
+    if eval_rg:
+        ious_rg[p_id]  = mmb.jc(prediction_mask_rg, ground_truth_mask)
+        dices_rg[p_id] = mmb.dc(prediction_mask_rg, ground_truth_mask)
+        rvds_rg[p_id]  = mmb.ravd(prediction_mask_rg, ground_truth_mask)
+        assds_rg[p_id] = mmb.assd(prediction_mask_rg, ground_truth_mask)
+        hds_rg[p_id]   = mmb.hd(prediction_mask_rg, ground_truth_mask)
 
-    rvds_pre[p_id]   = mmb.ravd(prediction_mask_pre, ground_truth_mask)
-    rvds_post[p_id]  = mmb.ravd(prediction_mask_post, ground_truth_mask)
+    if eval_cnn:
+        ious_pre[p_id]   = mmb.jc(prediction_mask_pre, ground_truth_mask)
+        ious_post[p_id]  = mmb.jc(prediction_mask_post, ground_truth_mask)
 
-    # Average SSD
-    assds_pre[p_id]  = mmb.assd(prediction_mask_pre, ground_truth_mask, voxelspacing=voxel_spacing)
-    assds_post[p_id] = mmb.assd(prediction_mask_post, ground_truth_mask, voxelspacing=voxel_spacing)
-    # Maximum SSD
-    hds_pre[p_id]    = mmb.hd(prediction_mask_pre, ground_truth_mask, voxelspacing=voxel_spacing)
-    hds_post[p_id]   = mmb.hd(prediction_mask_post, ground_truth_mask, voxelspacing=voxel_spacing)
+        dices_pre[p_id]  = mmb.dc(prediction_mask_pre, ground_truth_mask)
+        dices_post[p_id] = mmb.dc(prediction_mask_post, ground_truth_mask)
+
+        rvds_pre[p_id]   = mmb.ravd(prediction_mask_pre, ground_truth_mask)
+        rvds_post[p_id]  = mmb.ravd(prediction_mask_post, ground_truth_mask)
+
+        # Average SSD
+        assds_pre[p_id]  = mmb.assd(prediction_mask_pre, ground_truth_mask, voxelspacing=voxel_spacing)
+        assds_post[p_id] = mmb.assd(prediction_mask_post, ground_truth_mask, voxelspacing=voxel_spacing)
+        # Maximum SSD
+        hds_pre[p_id]    = mmb.hd(prediction_mask_pre, ground_truth_mask, voxelspacing=voxel_spacing)
+        hds_post[p_id]   = mmb.hd(prediction_mask_post, ground_truth_mask, voxelspacing=voxel_spacing)
 
 
 avg_iou_pre  = np.mean(ious_pre)
 avg_iou_post = np.mean(ious_post)
+avg_iou_rg   = np.mean(ious_rg)
 
 avg_dice_pre  = np.mean(dices_pre)
 avg_dice_post = np.mean(dices_post)
+avg_dice_rg   = np.mean(dices_rg)
 
 avg_rvd_pre  = np.mean(rvds_pre)
 avg_rvd_post = np.mean(rvds_post)
+avg_rvd_rg   = np.mean(rvds_rg)
 
 avg_assd_pre  = np.mean(assds_pre)
 avg_assd_post = np.mean(assds_post)
+avg_assd_rg   = np.mean(assds_rg)
 
 avg_hd_pre  = np.mean(hds_pre)
 avg_hd_post = np.mean(hds_post)
+avg_hd_rg   = np.mean(hds_rg)
 
-print("Average IoU  pre = {:.4f} post = {:.4f}".format(avg_iou_pre, avg_iou_post))
-print("Average Dice pre = {:.4f} post = {:.4f}".format(avg_dice_pre, avg_dice_post))
-print("Average RVD  pre = {:+.3f} post = {:+.3f}".format(avg_rvd_pre, avg_rvd_post))
-print("Average ASSD pre = {:.4f} post = {:.4f}".format(avg_assd_pre, avg_assd_post))
-print("Average HD   pre = {:.4f} post = {:.4f}".format(avg_hd_pre, avg_hd_post))
+print("Average IoU  pre = {:.4f} post = {:.4f} rg = {:.4f}".format(avg_iou_pre, avg_iou_post, avg_iou_rg))
+print("Average Dice pre = {:.4f} post = {:.4f} rg = {:.4f}".format(avg_dice_pre, avg_dice_post, avg_dice_rg))
+print("Average RVD  pre = {:+.3f} post = {:+.3f} rg = {:+.3f}".format(avg_rvd_pre, avg_rvd_post, avg_rvd_rg))
+print("Average ASSD pre = {:.4f} post = {:.4f} rg = {:.4f}".format(avg_assd_pre, avg_assd_post, avg_assd_rg))
+print("Average HD   pre = {:.4f} post = {:.4f} rg = {:.4f}".format(avg_hd_pre, avg_hd_post, avg_hd_rg))
 
 # TODO: remove line below
 # cpp_out = nib.load("F:/cpp_projects/__cpp_repos/example_app/out/build/x64-Release/segmentation-0.nii")
--- semseg/loss.py
+++ semseg/loss.py
@@ -15,12 +15,21 @@ def dice(outputs, labels):
 
 def tversky(outputs, labels, alpha=0.5, beta=0.5):
     eps = 1e-5
-    outputs, labels = outputs.float(), labels.float()
-    prob_0, gt_0 = outputs.flatten(), labels.flatten()
-    prob_1 = 1 - prob_0
-    gt_1 = torch.bitwise_not(gt_0)
-    tverksy_coeff = (torch.dot(prob_0, gt_0) + eps) \
-                     / (torch.dot(prob_0, gt_1) +
-                        alpha * torch.dot(prob_0, gt_1) + beta * torch.dot(prob_1, gt_0) + eps)
+    prob_0 = outputs.flatten()
+    prob_1 = 1-prob_0
+    gt_0 = labels.bool().flatten()
+    gt_1 = torch.bitwise_not(gt_0).flatten()
+    tverksy_coeff = (torch.dot(prob_0, gt_0.float()) + eps) \
+                     / (torch.dot(prob_0, gt_0.float()) +
+                        alpha * torch.dot(prob_0, gt_1.float()) + beta * torch.dot(prob_1, gt_0.float()) + eps)
     tversky_loss = 1 - tverksy_coeff
     return tversky_loss
+
+# Debug only
+""" 
+gt = torch.ones([5,16,16])
+pred = torch.ones([5,16,16]) * 0.95
+
+d = dice(pred, gt)
+t = tversky(pred, gt)
+"""
