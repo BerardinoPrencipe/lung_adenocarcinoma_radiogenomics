99694ff18bdb47330649485491607b6caa649974
Nicolik <33285381+Nicolik@users.noreply.github.com>
2020-02-01 19:40:25 +0100

    + patient exclusion

--- projects/liver/data_util/data_create_vessels.py
+++ projects/liver/data_util/data_create_vessels.py
@@ -22,6 +22,7 @@ else:
 
 # validation list
 val_list = ["{:02d}".format(idx) for idx in range(1,5)]
+exclude_list = ["10"]
 
 dataset_folder = os.path.join(current_path_abs, 'datasets/ircadb')
 # source folder where the .nii.gz files are located
@@ -74,31 +75,34 @@ for idx, subfolder_source in enumerate(os.listdir(source_folder)):
     new_image_filename = "volume-{}".format(id_patient)
     new_mask_filename = "segmentation-{}".format(id_patient)
 
-    # decide whether it will go to the train or val folder
-    sub = subfolders[1] if id_patient in val_list else subfolders[0]
+    if id_patient in exclude_list:
+        print('Excluding patient {}'.format(id_patient))
+    else:
+        # decide whether it will go to the train or val folder
+        sub = subfolders[1] if id_patient in val_list else subfolders[0]
 
-    # load file
-    image_data = nib.load(image_filename)
-    mask_data = nib.load(mask_filename)
+        # load file
+        image_data = nib.load(image_filename)
+        mask_data = nib.load(mask_filename)
 
-    # convert to numpy
-    image_data = image_data.get_data()
-    image_data = normalize_data(image_data, window_hu)
-    mask_data = mask_data.get_data()
-    mask_data_hv = (mask_data == VESSELS_CLASS[0]).astype(np.uint8)
-    mask_data_pv = (mask_data == VESSELS_CLASS[1]).astype(np.uint8)
-    mask_data = np.logical_or(mask_data_hv, mask_data_pv)
+        # convert to numpy
+        image_data = image_data.get_data()
+        image_data = normalize_data(image_data, window_hu)
+        mask_data = mask_data.get_data()
+        mask_data_hv = (mask_data == VESSELS_CLASS[0]).astype(np.uint8)
+        mask_data_pv = (mask_data == VESSELS_CLASS[1]).astype(np.uint8)
+        mask_data = np.logical_or(mask_data_hv, mask_data_pv)
 
-    # transpose so the z-axis (slices) are the first dimension
-    image_data = np.transpose(image_data, (2,0,1))
-    mask_data = np.transpose(mask_data, (2,0,1))
+        # transpose so the z-axis (slices) are the first dimension
+        image_data = np.transpose(image_data, (2,0,1))
+        mask_data = np.transpose(mask_data, (2,0,1))
 
-    # loop through the mask slices
-    for i, z_slice in enumerate(mask_data):
-        # save at new location (train or val)
-        np.save(os.path.join(destination_folder, sub, new_mask_filename + '_' + str(i)), z_slice)
+        # loop through the mask slices
+        for i, z_slice in enumerate(mask_data):
+            # save at new location (train or val)
+            np.save(os.path.join(destination_folder, sub, new_mask_filename + '_' + str(i)), z_slice)
 
-    # loop through the scan slices
-    for i, z_slice in enumerate(image_data):
-        # save at new location (train or val)
-        np.save(os.path.join(destination_folder, sub, new_image_filename + '_' + str(i)), z_slice)
+        # loop through the scan slices
+        for i, z_slice in enumerate(image_data):
+            # save at new location (train or val)
+            np.save(os.path.join(destination_folder, sub, new_image_filename + '_' + str(i)), z_slice)
