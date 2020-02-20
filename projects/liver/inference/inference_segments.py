1d7738bcee7b4cceb36ca37492b4bbf33acb2383
Nicola Altini <nicola.altini@poliba.it>
2020-02-20 10:39:43 +0100

    + Inference segments and vessels+tumors + TODO: refactor of code to make it more general

--- projects/liver/inference/inference_segments.py
+++ projects/liver/inference/inference_segments.py
@@ -27,6 +27,9 @@ else:
     path_net = os.path.join(current_path_abs, 'logs/segments/model_25D__2020-02-19__07_13_36.pht')
     net = torch.load(path_net)
 
+path_net_vessels = os.path.join(current_path_abs, 'logs/vessels_tumors/model_25D__2020-02-20__06_53_17.pht')
+net_vessels = torch.load(path_net_vessels)
+
 if torch.cuda.device_count() > 1:
     cuda_dev = torch.device('cuda:1')
 else:
@@ -36,16 +39,25 @@ print('Device Count = {}, using CUDA Device = {}'.format(torch.cuda.device_count
 net = net.cuda(cuda_dev)
 net.eval()
 
-print('Network Path = {}'.format(path_net))
+net_vessels = net_vessels.cuda(cuda_dev)
+net_vessels.eval()
+
+print('Network Path Segments = {}'.format(path_net))
+print('Network Path Vessels  = {}'.format(path_net_vessels))
+
 
 path_test_preds = list()
 
 folder_dataset     = 'E:/Datasets/LiverDecathlon/nii/images'
 folder_liver_masks = 'E:/Datasets/LiverDecathlon/nii/labels_liver'
-folder_prediction  = 'E:/Datasets/LiverDecathlon/nii/pred_segments'
+folder_prediction_segments = 'E:/Datasets/LiverDecathlon/nii/pred_segments'
+folder_prediction_vessels  = 'E:/Datasets/LiverDecathlon/nii/pred_vessels'
 
-if not os.path.exists(folder_prediction):
-    os.makedirs(folder_prediction)
+if not os.path.exists(folder_prediction_segments):
+    os.makedirs(folder_prediction_segments)
+
+if not os.path.exists(folder_prediction_vessels):
+    os.makedirs(folder_prediction_vessels)
 
 image_paths = []
 for idx in range(1,6):
@@ -59,9 +71,10 @@ image_paths += train_image_paths
 for idx, image_path in enumerate(image_paths):
 
     path_test_image = os.path.join(folder_dataset, image_path)
-    path_test_pred  = os.path.join(folder_prediction, image_path)
+    path_test_pred_segments = os.path.join(folder_prediction_segments, image_path)
+    path_test_pred_vessels  = os.path.join(folder_prediction_vessels, image_path)
     path_test_liver = os.path.join(folder_liver_masks, image_path)
-    path_test_preds.append(path_test_pred)
+    path_test_preds.append(path_test_pred_segments)
 
     # load file
     data = nib.load(path_test_image)
@@ -82,13 +95,26 @@ for idx, image_path in enumerate(image_paths):
     data = np.transpose(data, (2, 0, 1))
 
     # CNN
-    output = perform_inference_volumetric_image(net, data, context=2,
-                                                do_round=False, do_argmax=True, cuda_dev=cuda_dev)
-    output = np.transpose(output, (1, 2, 0)).astype(np.uint8)
+    output_segments = perform_inference_volumetric_image(net, data, context=2,
+                                                         do_round=False, do_argmax=True, cuda_dev=cuda_dev)
+    output_segments = np.transpose(output_segments, (1, 2, 0)).astype(np.uint8)
 
-    non_zero_elements = np.count_nonzero(output)
-    print("Non-Zero elements = {}".format(non_zero_elements))
+    output_vessels = perform_inference_volumetric_image(net_vessels, data, context=2,
+                                                        do_round=False, do_argmax=True, cuda_dev=cuda_dev)
+    output_vessels = np.transpose(output_vessels, (1,2,0)).astype(np.uint8)
 
-    output_nib = nib.Nifti1Image(output, affine=input_aff)
-    nib.save(output_nib, path_test_pred)
-    print("Index {} on {}.\nImage saved in {}".format(idx, len(image_paths)-1, path_test_pred))
+    non_zero_elements_segments = np.count_nonzero(output_segments)
+    print("Non-Zero elements segments = {}".format(non_zero_elements_segments))
+
+    non_zero_elements_vessels = np.count_nonzero(output_vessels)
+    print("Non-Zero elements vessels = {}".format(non_zero_elements_vessels))
+
+    output_nib_segments = nib.Nifti1Image(output_segments, affine=input_aff)
+    nib.save(output_nib_segments, path_test_pred_segments)
+
+    output_nib_vessels = nib.Nifti1Image(output_vessels, affine=input_aff)
+    nib.save(output_nib_vessels, path_test_pred_vessels)
+    print("Index {} on {}.\n"
+          "Segments Image saved in {}\n"
+          "Vessels Image saved  in {}".format(idx, len(image_paths) - 1,
+                                              path_test_pred_segments, path_test_pred_vessels))
