e4cd7c1e5dd14ca4ac8e5e0cdb18debc40b475d1
Nicola Altini <nicola.altini@poliba.it>
2020-06-17 17:35:15 +0200

    + icic vessels no dropout training (dice)

--- semseg/loss.py
+++ semseg/loss.py
@@ -36,7 +36,7 @@ def one_hot_encode(label, num_classes):
     :param num_classes: K classes
     :return: label_ohe, Tensor of shape BxKxHxW or BxKxDxHxW
     """
-    assert(len(label.shape) == 3 or len(label.shape) == 4, 'Invalid Label Shape {}'.format(label.shape))
+    assert len(label.shape) == 3 or len(label.shape) == 4, 'Invalid Label Shape {}'.format(label.shape)
     if len(label.shape) == 3:
         label_ohe = torch.zeros((label.shape[0], num_classes, label.shape[1], label.shape[2]))
     elif len(label.shape) == 4:
